68f3af8ff25a8966ec86ba1986d7c67988183c4a
Abir-Kharmachi <99890334+Abir-Kharmachi@users.noreply.github.com>
2025-04-09 18:10:50 +0200

    projetspring

--- src/main/java/com/tphedi/tp/model/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tphedi.tp.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
-}
